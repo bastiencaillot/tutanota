b50e8161376dc2859bc8285d5facbe8aba34735a
jom <jom@tutao.de>
2021-10-25 14:35:39 +0200

    improving error handling during date selection in calendar event drag, fix #3586

--- src/misc/MathUtils.js
+++ src/misc/MathUtils.js
@@ -1,9 +1,12 @@
 // @flow
 
-import {assertMainOrNodeBoot} from "../api/common/Env"
-
-assertMainOrNodeBoot()
-
 export function mod(n: number, m: number): number {
 	return ((n % m) + m) % m;
+}
+
+/**
+ * Clamp value to between min and max (inclusive). Min must be <= Max
+ */
+export function clamp(value: number, min: number, max: number): number {
+	return Math.max(min, Math.min(value, max))
 }
